e6999e1036fb5ef12f0e2d7b1f26386872c57d5e
John Henry <john@iamjohnhenry.com>
2015-10-21 13:42:25 -0700

    Align attributes in settings.

--- settings.js
+++ settings.js
@@ -1,17 +1,17 @@
 export default{
   SERVER : {
-      PORT      : process.env.PORT || 3000,
-      STATICDIR : './../client'
+      PORT            : process.env.PORT || 3000,
+      STATICDIR       : './../client'
   },
   CLIENT : {
-      SCRIPTS : ['./script.js'],
-      STYLES  : ['./style.css'],
-      INLINESCRIPTS : [],
-      INLINESTYLES  : []
+      SCRIPTS         : ['./script.js'],
+      STYLES          : ['./style.css'],
+      INLINESCRIPTS   : [],
+      INLINESTYLES    : []
   },
   BLOG : {
     ITEMSPERINDEXPAGE : 3,
-    DATADIR : './data',
-    REVERSE : false
+    DATADIR           : './data',
+    REVERSE           : false
   }
 };
