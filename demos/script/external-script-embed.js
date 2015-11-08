591c745fd9a1bc301fb3ffb113840826ff2db35d
John Henry <john@iamjohnhenry.com>
2015-11-08 14:26:10 -0800

    Remove unnecessary log

--- demos/script/external-script-embed.js
+++ demos/script/external-script-embed.js
@@ -10,7 +10,6 @@ export default s =>{
   }else{
     code = fs.readFileSync(s.source, 'utf-8');
   }
-  console.log(code);
   return s.type === 'style' ? `${(s.amp ? '<style>body {opacity: 0}</style><noscript><style>body {opacity: 1}</style></noscript>' : '')}<style rel="stylesheet"${(s.amp ? ' amp-custom' : '')}>${code}</style>` :
   `<script${(s.async? ' async' : '')}${(s.defer? ' defer' : '')}>${code}</script>`
 };
