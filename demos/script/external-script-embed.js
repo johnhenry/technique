e951a9dc9cbf852caefdde560587aa74d35336e3
John Henry <john@iamjohnhenry.com>
2015-11-08 14:19:34 -0800

    Enable embedding of remote scripts

--- demos/script/external-script-embed.js
+++ demos/script/external-script-embed.js
@@ -1,6 +1,16 @@
 import fs from 'fs';
+import request from 'sync-request';
 export default s =>{
-  const code = fs.readFileSync(s.source, 'utf-8');
+  var code;
+  if(s.source.indexOf('http') !== -1){
+    var source =  s.source.substring(s.source.indexOf('http'));
+    source = source.replace('http:/', 'http://');
+    source = source.replace('https:/', 'https://');
+    code = String(request('GET', source).getBody());
+  }else{
+    code = fs.readFileSync(s.source, 'utf-8');
+  }
+  console.log(code);
   return s.type === 'style' ? `${(s.amp ? '<style>body {opacity: 0}</style><noscript><style>body {opacity: 1}</style></noscript>' : '')}<style rel="stylesheet"${(s.amp ? ' amp-custom' : '')}>${code}</style>` :
   `<script${(s.async? ' async' : '')}${(s.defer? ' defer' : '')}>${code}</script>`
 };
