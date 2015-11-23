cfdf78f7bf6d5bda00ee04fd86a08ddeab958ded
John Henry <john@iamjohnhenry.com>
2015-11-23 12:33:35 -0800

    Change 'state' to update in state-todo-server

--- demos/controller/todo-server.js
+++ demos/controller/todo-server.js
@@ -1,4 +1,4 @@
-import state from '../script/state-todo-server';
+import update from '../script/state-todo-server';
 export var subscriptions = [];
-export const send = action => state(action)
+export const send = action => update(action)
   .then(data => subscriptions.forEach(subscription => subscription(data)));
